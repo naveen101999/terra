c31a4259adf831bf85031684a2951f3f07bdcc11
naveen101999 <93638367+naveen101999@users.noreply.github.com>
2022-01-05 16:00:04 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+profile ="default"
+region ="us-east-1"
+}
+resource "aws_instance" "myec2"{
+ami            =" ami-04505e74c0741db8d"
+instance_type  ="t2.micro"
+iam_instance_profile ="demo-Role"
+key_name       ="Naveen06training"
+subnet_id    ="subnet-010656811f3381d92"
+tags = {
+      Name ="helloworld"
+   }
+ }
