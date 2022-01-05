5df533184d9e1ff9d4a5d6efe97ae15603a26b52
naveen101999 <93638367+naveen101999@users.noreply.github.com>
2022-01-05 16:31:57 +0530

    Update my_terra.tf

--- my_terra.tf
+++ my_terra.tf
@@ -3,7 +3,7 @@ profile ="default"
 region ="us-east-1"
 }
 resource "aws_instance" "terra" {
-ami            =" ami-04505e74c0741db8d"
+ami            ="ami-04505e74c0741db8d"
 instance_type  ="t2.micro"
 iam_instance_profile ="demo-Role"
 key_name       ="Naveen06training"
